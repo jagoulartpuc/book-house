73c5c5970f647a73971bab4d4895b9cacf0c18dc
Jose Goulart <jose.goulart@acad.pucrs.br>
2023-03-03 13:32:18 -0300

    add logs

--- src/main/java/com/bideafactory/book/controller/BookController.java
+++ src/main/java/com/bideafactory/book/controller/BookController.java
@@ -3,6 +3,8 @@ package com.bideafactory.book.controller;
 import com.bideafactory.book.domain.Book;
 import com.bideafactory.book.exception.InvalidDiscountException;
 import com.bideafactory.book.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +21,16 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    private static Logger LOGGER = LoggerFactory.getLogger(BookController.class);
+
     @PostMapping("/book")
     public ResponseEntity<String> postBook(@RequestBody Book book) {
         try {
             bookService.insertBook(book);
+            LOGGER.info("Book created successfully: " + book);
             return ResponseEntity.ok("Book Accepted");
         } catch (InvalidDiscountException | IOException | InterruptedException e) {
+            LOGGER.error("Error in book " + book.getId() + ": " + e.getMessage());
             return ResponseEntity.status(409).body("Invalid discount");
         }
     }
